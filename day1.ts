6f66d7e26bdae0d06ed84d26a5158f117036ee82
Ethan Arrowood <ethan.arrowood@gmail.com>
2019-12-01 18:14:11 -0500

    day 1 complete ⛄️

---
+++ day1.ts
@@ -0,0 +1,21 @@
+import fs from 'fs'
+const fp = fs.promises
+import path from 'path'
+
+(async () => {
+  const calcFuelMass = (currentMass: number): number => {
+    const nextFuelMass = Math.floor(currentMass / 3) - 2
+    return nextFuelMass < 0 ? 0 : nextFuelMass + calcFuelMass(nextFuelMass)
+  }
+  try {
+    const inputData = await fp.readFile(path.join(__dirname, './input'), { encoding: 'utf8' })
+    const input = inputData.split('\n') // don't love ingesting data like this but the input was small so this is okay
+    const total = input.reduce((acc, mass) => {
+      // return acc + Math.floor(Number.parseInt(mass) / 3) - 2 // Part 1
+      return acc + calcFuelMass(Number.parseInt(mass)) // Part 2
+    }, 0)
+    console.log(`Total sum: ${total}`)
+  } catch (err) {
+    console.error(err)
+  }
+})()
